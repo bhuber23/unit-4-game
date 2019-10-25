6905cb1906407fe3848f488b4a3375fb971d795f
Brittany Huber <brittanyleighhuber@gmail.com>
2019-10-24 22:14:18 -0600

    fixed issue with number not generating on refresh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,43 +3,81 @@ var reality = Math.floor(Math.random() * 11) + 1;
 var space = Math.floor(Math.random() * 11) + 1;
 var power = Math.floor(Math.random() * 11) + 1;
 var mind = Math.floor(Math.random() * 11) + 1;
+var time = Math.floor(Math.random() * 11) + 1;
+var soul = Math.floor(Math.random() * 11) + 1;
 var wins = 0;
 var losses = 0;
 var randomNumber = Math.floor(Math.random() * 101) + 19;
 var yourNumber = 0;
 
+//Function to choose number
+function numberGenerator() {
+    randomNumber = Math.floor(Math.random() * 101) + 19;
+    $("#random-number").html(randomNumber);
+}
+
+numberGenerator();
+
 //On-click functions for stones
 $("#reality-stone").on("click", function () {
     yourNumber = yourNumber + reality;
-    $("#score").text(yourNumber);
+    $("#your-number").text(yourNumber);
+    endGame();
     
 });
 
 $("#space-stone").on("click", function () {
     yourNumber = yourNumber + space;
-    $("#score").text(yourNumber);
+    $("#your-number").text(yourNumber);
+    endGame();
 })
 
 $("#power-stone").on("click", function () {
     yourNumber = yourNumber + power;
-    $("#score").text(yourNumber);
+    $("#your-number").text(yourNumber);
+    endGame();
 })
 
 $("#mind-stone").on("click", function () {
     yourNumber = yourNumber + mind;
-    $("#score").text(yourNumber);
+    $("#your-number").text(yourNumber);
+    endGame();
 })
 
-//To keep score
-$("#random-number").html(randomNumber);
+$("#time-stone").on("click", function () {
+    yourNumber = yourNumber + time;
+    $("#your-number").text(yourNumber);
+    endGame();
+})
 
+$("#soul-stone").on("click", function () {
+    yourNumber = yourNumber + mind;
+    $("#your-number").text(yourNumber);
+    endGame();
+})
+
+
+
+//When the game is won/lost
+function endGame() {
 if (yourNumber === randomNumber) {
     wins ++;
     $("#win-count").text(wins);
+    reset()
 }
+
 else if (yourNumber > randomNumber) {
     losses++;
     $("#loss-count").text(losses);
+    reset();
     
+}};
+
+//Functions
+function reset () {
+    yourNumber = 0;
+    $("#your-number").text(yourNumber);
+    numberGenerator();
 }
+
 
